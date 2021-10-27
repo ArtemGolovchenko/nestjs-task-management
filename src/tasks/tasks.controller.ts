0488a5bdbe5143ee031b88ade02ad3f8b652f6bf
Artem <art2937@gmail.com>
2021-10-27 17:44:52 +0300

    create connection to database

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,43 +9,4 @@ import { TasksService } from './tasks.service';
 export class TasksController {
     constructor(private tasksService: TasksService) { }
 
-    @Get()
-    getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
-        if (Object.keys(filterDto).length) {
-            return this.tasksService.getTasksFithFilters(filterDto);
-        }
-        else {
-            return this.tasksService.getAllTasks();
-        }
-    }
-    //
-    @Get('/all')
-    getAllTasks(): Task[] {
-        return this.tasksService.getAllTasks();
-    }
-    //
-
-    @Get('/:id')
-    getTaskById(@Param('id') id: string): Task {
-        return this.tasksService.getTaskById(id);
-    }
-
-    @Post()
-    createTask(@Body() createTaskDto: CreateTaskDto): Task {
-        return this.tasksService.createTask(createTaskDto);
-    }
-
-    @Delete('/:id')
-    deleteTask(@Param('id') id: string): void {
-        return this.tasksService.deleteTask(id);
-    }
-
-    @Patch('/:id/status')
-    updateTaskStatus(
-        @Param('id') id: string,
-        @Body() updateTaskStatusDto: updateTaskStatusDto,
-    ): Task {
-        const { status } = updateTaskStatusDto;
-        return this.tasksService.updateTaskStatus(id, status);
-    }
 }
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/database.module';
 import { TasksController } from './tasks.controller';
+import { tasksProviders } from './tasks.providers';
 import { TasksService } from './tasks.service';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [
+    TasksService,
+    ...tasksProviders,
+  ],
 })
 export class TasksModule {}
